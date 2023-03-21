5bc609f318106db33b5e6b74bf5e3f97ade25900
Fyamada1229 <yamadafutosi1229@gmail.com>
2023-03-21 23:46:49 +0900

    新規に作り直しました。
    routerの設定、新しくUsersControllerを追加しました。

--- src/laravel/resources/views/welcome.blade.php
+++ src/laravel/resources/views/welcome.blade.php
@@ -1,16 +1,1 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
-    </style>
-</head>
-
-<body class="antialiased">
-    <h1>Hello wrold </h1>
-</body>
-
-</html>
+<h1>404</h1>
