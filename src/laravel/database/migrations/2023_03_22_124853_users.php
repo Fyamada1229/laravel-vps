d2b490c02ab1c473a54eef6da3fc6df45d6edceb
Fyamada1229 <yamadafutosi1229@gmail.com>
2023-03-22 23:00:50 +0900

    controller作成、seeder作成

---
+++ src/laravel/database/migrations/2023_03_22_124853_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id')->comment('ユーザーID');
+            $table->string('name')->comment('名前');
+            $table->string('email')->comment('メールアドレス');
+            $table->string('password')->comment('パスワード');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
--- src/laravel/resources/views/welcome.blade.php
+++ src/laravel/resources/views/welcome.blade.php
@@ -1,1 +1,24 @@
-<h1>404</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Error Page</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h1>404</h1>
+                <h2>Oops! Page Not Found</h2>
+                <p>The page you are looking for might have been removed, had its name changed or is temporarily unavailable.</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
