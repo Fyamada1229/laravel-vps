e5fd1d0f497be2054770628838132c68cd2f3ee4
Fyamada1229 <yamadafutosi1229@gmail.com>
2023-03-24 22:36:09 +0900

    最初のコミット

--- src/laravel/routes/api.php
+++ src/laravel/routes/api.php
@@ -2,12 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\UsersController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('users', [UsersController::class, 'index']);
-Route::get('users/{id}', [UsersController::class, 'show']);
-Route::post('store', [UsersController::class, 'store']);
